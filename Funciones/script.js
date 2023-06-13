0f1ddfddf7d4c1cbeec1b533e966f50c8540c879
CelesteSelena2022 <109759909+CelesteSelena2022@users.noreply.github.com>
2023-06-13 20:46:21 -0300

    modifico carpeta de la clase 1  agrego ejercicios de la clase 2

---
+++ Funciones/script.js
@@ -0,0 +1,25 @@
+//gritar(str) palabra + parentesis = funcion------------------------------------------------
+//Crear una función gritar que tome como argumento un string y devuelva el mismo string con un signo de exclamación al principio y al final del mismo
+//gritar('hola') // ¡hola!
+//gritar('aaaaaa') // ¡aaaaaa!
+
+// const gritar = (str) => {
+//     return `¡${str}!` 
+// }
+
+// console.log(gritar(`hola`))
+// console.log(gritar(`aaaaaa`))
+
+// obtenerDatosDeCiudad(nombre, poblacion, pais)------------------------------------------------
+// Crear una función obtenerDatosDeCiudad que tome como argumentos un string nombre, un número poblacion y un string pais y devuelva string con el siguiente formato: La ciudad de NOMBRE tiene una población de POBLACION habitantes y está ubicada en PAIS
+// obtenerDatosDeCiudad('Santa Fe', 545606, 'Argentina') // 'La ciudad de Santa Fe tiene una población de 545606 habitantes y está ubicada en Argentina'
+
+// const obtenerDatosDeCiudad = () => {
+//     let ciudad = prompt('Ingrese su ciudad');
+//     let habitantes = Number(prompt('Ingrese cantidad de habitantes'));
+//     let country = prompt('Ingrese el pais');
+//     return `La ciudad de ${ciudad} tiene una población de ${habitantes} habitantes y está ubicada en ${country}`
+// };
+
+// console.log(obtenerDatosDeCiudad());
+
---
+++ VARIABLES, DATOS Y OPERADORES/script.js
@@ -0,0 +1,35 @@
+//Saludo--------------------------------------------------------
+//Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
+
+// let nombre = prompt ("ingrese su nombre")
+// let apellido = prompt ("ingrese su apellido")
+// const saludo= "bienvenida a Ada"
+
+// alert (`Hola ${nombre} ${apellido}, ${saludo}`)
+
+//Heladería--------------------------------------------------------
+// Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada".
+
+// let primerSabor = prompt ("ingrese el primer sabor de helado");
+// let segundoSabor = prompt ("ingrese segundo sabor");
+// let tercerSabor = prompt ("ingrese el tercer sabor");
+
+// alert (`Aqui tiene su helado de ${primerSabor}, ${segundoSabor} y ${tercerSabor}`);
+
+//Datos personales--------------------------------------------------------
+//Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos.
+
+
+
+//Años perro--------------------------------------------------------
+//Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro.
+
+// let edad = prompt ("ingrese la edad", "0");
+// const aniosPerros = 7;
+// let resultado = Number(edad) * aniosPerros;
+
+// alert (`Tu edad en años perros es ${resultado} años`);
+
+
+
+
